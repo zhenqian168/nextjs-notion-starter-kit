b26127dcc36a5aa9a0bede74b7df38d13ff301c9
RockyGuo <46557006+zhenqian168@users.noreply.github.com>
2023-11-14 18:01:43 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,16 +2,16 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '40e21b202d144eafb15d447c064ba7a1',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: '效率工具指南',
+  domain: 'https://www.notion.so/rockyguo/40e21b202d144eafb15d447c064ba7a1?v=95d7637bf6704a008c84286c81a59b36&pvs=4',
+  author: 'Rocky Guo',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
